55782685f947ff421a44474ea8333748d66bce03
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2022-10-30 22:23:44 +0300

    before deployment

--- JS/crome-extension-project.js
+++ JS/crome-extension-project.js
@@ -3,15 +3,22 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", () => {
+inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     renderLeads()
 })
 
-const renderLeads = () => {
+function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems  
 }
