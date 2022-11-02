dbdcb25777be4d4404a559a94dda0b0d13caa365
MEHMET SORKULU <112237375+ZorkoMehmet@users.noreply.github.com>
2022-11-02 20:47:19 +0300

    splitting the url

--- JS/crome-extension-project.js
+++ JS/crome-extension-project.js
@@ -1,5 +1,5 @@
 let products = []
-
+let pairs = {}
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -14,22 +14,26 @@ if (leadsFromLocalStorage) {
 
 tabBtn.addEventListener("click", function(){    
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        products.push(tabs[0].url)
+        pairs = {
+            'tabs[0].url.split("/")[5]': "tabs[0].url",
+        };
+
+        products.push(pairs)
         localStorage.setItem("products", JSON.stringify(products) )
         render(products)
     })
 })
 
-let url = "https://www.amazon.ca/Sponge-Bottle-Brush-Pack-Blue/dp/B07DQQFP63/ref=pd_day0fbt_img_sccl_2/135-2216868-0547034?pd_rd_w=hioy6&content-id=amzn1.sym.2788fe34-0865-4f82-b5fb-522b9cf2f5fd&pf_rd_p=2788fe34-0865-4f82-b5fb-522b9cf2f5fd&pf_rd_r=02HTF0WGY43Y894PN7W1&pd_rd_wg=oUKN6&pd_rd_r=b08549df-cf72-4e5d-84dd-695c9bf3dd48&pd_rd_i=B07DQQFP63&psc=1"
-console.log(url.split('/')[5]); 
+// let url = "https://www.amazon.ca/Sponge-Bottle-Brush-Pack-Blue/dp/B07DQQFP63/ref=pd_day0fbt_img_sccl_2/135-2216868-0547034?pd_rd_w=hioy6&content-id=amzn1.sym.2788fe34-0865-4f82-b5fb-522b9cf2f5fd&pf_rd_p=2788fe34-0865-4f82-b5fb-522b9cf2f5fd&pf_rd_r=02HTF0WGY43Y894PN7W1&pd_rd_wg=oUKN6&pd_rd_r=b08549df-cf72-4e5d-84dd-695c9bf3dd48&pd_rd_i=B07DQQFP63&psc=1"
+// console.log(url.split('/')[5]); 
 
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
+                <a target='_blank' href='${leads[i].key}'>
+                    ${leads[i].key}
                 </a>
             </li>
         `
